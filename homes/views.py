84d2904afc96632b0b185b2736fd987037f54fa5
Chase Gindlesperger <cmg190@zips.uakron.edu>
2020-10-27 14:20:14 -0400

    Added search function to /homes/index

--- homes/views.py
+++ homes/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from homes.models import House, Apartment, Food, Amenities
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("This is the housing index")
+    context = {}
+    return render(request, 'homes/index.html', context)
+
+def search(request):
+    if request.method == "GET":
+        search_value = request.GET.get('search')
+        queryset = Apartment.objects.filter(Name__icontains = search_value)
+        return render(request, 'homes/searchresults.html', {'homes_list': queryset})
