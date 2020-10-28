da02d21cb47045f830103ae46f870290161dc26c
Chase Gindlesperger <cmg190@zips.uakron.edu>
2020-10-27 20:39:35 -0400

    Fixed DB model issues

--- homes/models.py
+++ homes/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+
 class Amenities(models.Model):
-    Address = models.CharField(max_length=50, primary_key=True)
+    Address = models.CharField(max_length=50)
     Laundry = models.CharField(null = False, default='N/A', max_length=50, choices=[('None', 'None'), ('Room', 'Room'), ('Building','Building')])
     Kitchen = models.CharField(null = False, default='N/A', max_length=50, choices=[('None', 'None'), ('Half', 'Half'), ('Full', 'Full')])
     CommonAreas =models.CharField(null = False, default='N/A', max_length=50, choices=[('None', 'None'), ('Few', 'Few'), ('Many', 'Many')])
@@ -14,7 +15,20 @@ class Amenities(models.Model):
         return self.Address
 
 class House(models.Model):
-    Address = models.ForeignKey(Amenities, on_delete= models.CASCADE)
+    amenities = models.ForeignKey(Amenities, on_delete= models.CASCADE, default='0')
+    Address = models.CharField(max_length=50)
+    Bedrooms = models.IntegerField(null= False)
+    Bathrooms = models.DecimalField(null = False, max_digits = 2, decimal_places = 1)
+    Price = models.IntegerField(null = False)
+    Safety = models.CharField(null = False, default='N/A', max_length=50, choices=[('Least Safe', 'Least Safe'), ('Less Safe', 'Less Safe'), ('More Safe', 'More Safe'), ('Most Safe', 'Most Safe')])
+    image = models.ImageField(upload_to='images/', default='default.jpg')
+    def __str__(self):
+        return self.Address
+
+class Apartment(models.Model):
+    amenities = models.ForeignKey(Amenities, on_delete= models.CASCADE, default='0')
+    Address = models.CharField(max_length=50)
+    Name = models.CharField(max_length=50, null= False)
     Bedrooms = models.IntegerField(null= False)
     Bathrooms = models.DecimalField(null = False, max_digits = 2, decimal_places = 1)
     Price = models.IntegerField(null = False)
@@ -23,25 +37,15 @@ class House(models.Model):
     def __str__(self):
         return self.Name
 
-class Apartment(models.Model):
-    Address = models.ForeignKey(Amenities, on_delete= models.CASCADE)
-    Name = models.CharField(max_length=50, null= False)
-    Bedrooms = models.IntegerField(null= False, primary_key= True)
-    Bathrooms = models.DecimalField(null = False, max_digits = 2, decimal_places = 1)
-    Price = models.IntegerField(null = False)
-    Safety = models.CharField(null = False, default='N/A', max_length=50, choices=[('Least Safe', 'Least Safe'), ('Less Safe', 'Less Safe'), ('More Safe', 'More Safe'), ('Most Safe', 'Most Safe')])
-    image = models.ImageField(upload_to='images/', default='default.jpg')
-    def __str__(self):
-        return self.Name
-
 class Food(models.Model):
-    Address = models.CharField(max_length=50, primary_key= True)
+    Address = models.CharField(max_length=50)
     Name = models.CharField(max_length=50, null = False)
     def __str__ (self):
         return self.Name
 
 class Dorms(models.Model):
-    Address = models.ForeignKey(Amenities, on_delete= models.CASCADE)
+    amenities = models.ForeignKey(Amenities, on_delete= models.CASCADE, default='0')
+    Address = models.CharField(max_length=50)
     Name = models.CharField(max_length=50, null= False)
     Roomstyle = models.CharField(null = False, default='N/A', max_length=50, choices=[('Single', 'Single'), ('Shared Single', 'Shared Single'), ('Double', 'Double'), ('Triple', 'Triple')])
     Bathrooms = models.CharField(null = False, default='N/A', max_length=50, choices=[('Private', 'Private'), ('Communal', 'Communal')])
--- homes/views.py
+++ homes/views.py
@@ -1,15 +1,21 @@
+from itertools import chain
 from django.shortcuts import render
-from django.http import HttpResponse
-from homes.models import House, Apartment, Food, Amenities
+from django.db.models import QuerySet
+from homes.models import House, Apartment, Dorms, Amenities
 
 # Create your views here.
 
 def index(request):
-    context = {}
-    return render(request, 'homes/index.html', context)
+    return render(request, 'homes/index.html')
 
 def search(request):
-    if request.method == "GET":
-        search_value = request.GET.get('search')
-        queryset = Apartment.objects.filter(Name__icontains = search_value)
-        return render(request, 'homes/searchresults.html', {'homes_list': queryset})
+    houses = QuerySet()
+    apartments = QuerySet()
+    dorms = QuerySet()
+    result = render(request, 'homes/index.html')
+    if request.method == 'GET':
+        if 'search' in request.GET:
+            search_value = request.GET.get('search')
+            if search_value is not None and search_value != '':
+                result = Apartment.objects.filter(Address__Address__icontains = search_value)
+    return result
