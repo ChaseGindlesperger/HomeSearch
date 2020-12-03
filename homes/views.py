bb698b706794f5ee5c530579b0b61c82c9846161
Chase Gindlesperger <cmg190@zips.uakron.edu>
2020-12-03 14:45:15 -0500

    Added amenity filtering

--- homes/views.py
+++ homes/views.py
@@ -18,17 +18,7 @@ def index(request):
 
 
 def info(request):
-    home = request.GET.get('home')
-<<<<<<< HEAD
-    if House.objects.filter(Address = home).count():
-        info = House.objects.get(Address = home)
-    elif Apartment.objects.filter(Name = home).count() > 0:
-        info = Apartment.objects.get(Name = home)
-    elif Dorms.objects.filter(Name = home).count() > 0:
-        info = Dorm.objects.get(Name = home)
-
-    return render(request, 'homes/info.html', {'home':info})
-=======
+    home = request.GET.get('home');
     if House.objects.filter(Address=home).count():
         data = House.objects.get(Address=home)
     elif Apartment.objects.filter(Name=home).count() > 0:
@@ -42,7 +32,6 @@ def info(request):
     #json_list = json.dumps(food_list)
     return render(request, 'homes/info.html', {'home': data, 'food_list': food})
 
->>>>>>> fb5f2a97511db32cfa080085f8e88e710f9168d2
 
 def search(request):
     houses = QuerySet()
@@ -53,17 +42,29 @@ def search(request):
     price_select = ''
     price_bool = False
     bedroom_bool = False
+    laundry_bool = False
+    kitchen_bool = False
+    AC_bool = False
     if request.method == "GET":
         bedroom_value = request.GET.get('bedroomselect')
-        if bedroom_value is not None and bedroom_value != '':
+        if bedroom_value is not None and bedroom_value != '' and bedroom_value != 'Any':
             bedroom_bool = True
+        laundry_value = request.GET.get('laundry');
+        if laundry_value is not None and laundry_value != '' and laundry_value != 'Any':
+            laundry_bool = True
         price_value = request.GET.get('price')
         price_select = request.GET.get('priceselect')
+        if price_value is not None and price_value != '':
+            price_bool = True
+        kitchen_value = request.GET.get('kitchen')
+        if kitchen_value is not None and kitchen_value != '' and kitchen_value != 'Any':
+            kitchen_bool = True
+        AC_value = request.GET.get('AC')
+        if AC_value is not None and AC_value != '' and AC_value != 'Any':
+            AC_bool = True   
         if 'search' in request.GET:
             if request.GET.get('search') is not None and request.GET.get('search') != '':
                 search_value = request.GET.get('search')
-        if price_value is not None and price_value != '':
-            price_bool = True
         if 'type' in request.GET:
             type_value = request.GET.get('type')
             if type_value is not None and type_value != '':
@@ -73,6 +74,12 @@ def search(request):
                         houses = filters.filter_price(houses, price_value, price_select)
                     if bedroom_bool:
                         houses = houses.filter(Bedrooms=bedroom_value)
+                    if laundry_bool:
+                        houses = houses.filter(amenities__Laundry=laundry_value)
+                    if kitchen_bool:
+                        houses = houses.filter(amenities__Kitchen=kitchen_value)
+                    if AC_bool:
+                        houses = houses.filter(amenities__AC=AC_value)
                     result = render(request, 'homes/searchresults.html', {'homes_list': houses})
                 elif type_value == 'Apartment':
                     apartments = Apartment.objects.filter(Address__icontains=search_value)
@@ -80,11 +87,23 @@ def search(request):
                         apartments = filters.filter_price(apartments, price_value, price_select)
                     if bedroom_bool:
                         apartments = apartments.filter(Bedrooms=bedroom_value)
+                    if laundry_bool:
+                        apartments = apartments.filter(amenities__Laundry=laundry_value)
+                    if kitchen_bool:
+                        apartments = apartments.filter(amenities__Kitchen=kitchen_value)
+                    if AC_bool:
+                        apartments = apartments.filter(amenities__AC=AC_value)
                     result = render(request, 'homes/searchresults.html', {'homes_list': apartments})
                 elif type_value == 'Dorm':
                     dorms = Dorms.objects.filter(Address__icontains=search_value)
                     if price_bool:
                         dorms = filters.filter_price(dorms, price_value, price_select)
+                    if laundry_bool:
+                        dorms = dorms.filter(amenities__Laundry=laundry_value)
+                    if kitchen_bool:
+                        dorms = dorms.filter(amenities__Kitchen=kitchen_value)
+                    if AC_bool:
+                        dorms = dorms.filter(amenities__AC=AC_value)
                     result = render(request, 'homes/searchresults.html', {'homes_list': dorms})
                 else:
                     houses = House.objects.filter(Address__icontains=search_value)
@@ -92,14 +111,32 @@ def search(request):
                         houses = filters.filter_price(houses, price_value, price_select)
                     if bedroom_bool:
                         houses = houses.filter(Bedrooms=bedroom_value)
+                    if laundry_bool:
+                        houses = houses.filter(amenities__Laundry=laundry_value)
+                    if kitchen_bool:
+                        houses = houses.filter(amenities__Kitchen=kitchen_value)
+                    if AC_bool:
+                        houses = houses.filter(amenities__AC=AC_value)
                     apartments = Apartment.objects.filter(Address__icontains=search_value)
                     if price_bool:
                         apartments = filters.filter_price(apartments, price_value, price_select)
                     if bedroom_bool:
                         apartments = apartments.filter(Bedrooms=bedroom_value)
+                    if laundry_bool:
+                        apartments = apartments.filter(amenities__Laundry=laundry_value)
+                    if kitchen_bool:
+                        apartments = apartments.filter(amenities__Kitchen=kitchen_value)
+                    if AC_bool:
+                        apartments = apartments.filter(amenities__AC=AC_value)
                     dorms = Dorms.objects.filter(Address__icontains=search_value)
                     if price_bool:
                         dorms = filters.filter_price(dorms, price_value, price_select)
+                    if laundry_bool:
+                        dorms = dorms.filter(amenities__Laundry=laundry_value)
+                    if kitchen_bool:
+                        dorms = dorms.filter(amenities__Kitchen=kitchen_value)
+                    if AC_bool:
+                        dorms = dorms.filter(amenities__AC=AC_value)
                     queryset = chain(houses, apartments, dorms)
                     result = render(request, 'homes/searchresults.html', {'homes_list': queryset})
     return result
